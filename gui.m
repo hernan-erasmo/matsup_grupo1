9c4a8eb4e4bf1a5d40b2d86dc8532fc3267bc171
Hernán Erasmo <dherasmo@gmail.com>
2017-05-05 11:37:02 -0300

    Arregla indentación

--- gui.m
+++ gui.m
@@ -4,16 +4,14 @@
 f = figure("MenuBar", "None");
 
 btn_ingresar = uimenu(f, "Label", "Ingresar nueva G(s)");
-
 btn_funciones = uimenu(f, "Label", "Funciones");
-btn_detalles_completos = uimenu(btn_funciones, "Label", "Mostrar detalles completos");
-btn_caract_particular = uimenu(btn_funciones, "Label", "Elegir caracteristica en particular");
-btn_expresion = uimenu(btn_caract_particular, "Label", "Obtener expresion de G(s)");
-btn_polos = uimenu(btn_caract_particular, "Label", "Indicar Polos");
-btn_ceros = uimenu(btn_caract_particular, "Label", "Indicar Ceros");
-btn_ganancia = uimenu(btn_caract_particular, "Label", "Indicar ganancia de G(s)");
-btn_pcg = uimenu(btn_caract_particular, "Label", "Indicar polos, ceros y ganancia");
-btn_graf_pc = uimenu(btn_caract_particular, "Label", "Graficar polos y ceros");
-btn_estabilidad = uimenu(btn_caract_particular, "Label", "Indicar estabilidad");
-
+  btn_detalles_completos = uimenu(btn_funciones, "Label", "Mostrar detalles completos");
+  btn_caract_particular = uimenu(btn_funciones, "Label", "Elegir caracteristica en particular");
+    btn_expresion = uimenu(btn_caract_particular, "Label", "Obtener expresion de G(s)");
+    btn_polos = uimenu(btn_caract_particular, "Label", "Indicar Polos");
+    btn_ceros = uimenu(btn_caract_particular, "Label", "Indicar Ceros");
+    btn_ganancia = uimenu(btn_caract_particular, "Label", "Indicar ganancia de G(s)");
+    btn_pcg = uimenu(btn_caract_particular, "Label", "Indicar polos, ceros y ganancia");
+    btn_graf_pc = uimenu(btn_caract_particular, "Label", "Graficar polos y ceros");
+    btn_estabilidad = uimenu(btn_caract_particular, "Label", "Indicar estabilidad");
 btn_salir = uimenu(f, "Label", "Salir");
