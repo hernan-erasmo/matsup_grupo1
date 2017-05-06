94b2b1198d9465325bdd4204e14364ac4a272d2b
Hernán Erasmo <dherasmo@gmail.com>
2017-05-06 16:28:07 -0300

    Agrega el codigo de las funciones

---
+++ funciones.m
@@ -0,0 +1,8 @@
+1; % Para que no ejecute el archivo en cuanto se cargue con 'source' desde gui.m
+
+function dlg_ingresar_gs
+  prompt = {"Numerador", "Denominador"};
+  defaults = {"2,3", "1,0,4,3"};
+  rowscols = 1;
+  dims = inputdlg (prompt, "Ingrese coeficientes de G(s)", rowscols, defaults);
+endfunction
