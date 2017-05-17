9a1216e9fd5d760a251cb4e58562482c4172740c
Hernán Erasmo <derasmo@autotrol.com.ar>
2017-05-17 12:15:48 -0300

    Asigna tf a var. gobal a partir de ingreso de coef.
    Ahora se puede ingresar una función de transferencia a partir de los
    coeficientes del numerador y denominador. La misma se carga como
    función de transferencia (objeto de octave) en una variable global.

--- funciones.m
+++ funciones.m
@@ -1,8 +1,16 @@
 1; % Para que no ejecute el archivo en cuanto se cargue con 'source' desde gui.m
 
+global func_actual;
+
 function dlg_ingresar_coef
   prompt = {"Numerador (separados por comas)", "Denominador (separados por comas)"};
   defaults = {"2,3", "1,0,4,3"};
   rowscols = 1;
   dims = inputdlg(prompt, "Ingrese coeficientes de G(s)", rowscols, defaults);
+
+  num = str2double(strsplit(dims{1},","));
+  den = str2double(strsplit(dims{2},","));
+
+  global func_actual;
+  func_actual = tf(num,den);
 endfunction
