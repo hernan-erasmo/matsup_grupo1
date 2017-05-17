455aa8c24ca98d540ecdc5964f2baeba68715c51
Hernán Erasmo <derasmo@autotrol.com.ar>
2017-05-17 11:01:22 -0300

    Cambia labels en inputdlg de ingreso de coefs. G(s)

--- funciones.m
+++ funciones.m
@@ -1,8 +1,8 @@
 1; % Para que no ejecute el archivo en cuanto se cargue con 'source' desde gui.m
 
 function dlg_ingresar_coef
-  prompt = {"Numerador", "Denominador"};
+  prompt = {"Numerador (separados por comas)", "Denominador (separados por comas)"};
   defaults = {"2,3", "1,0,4,3"};
   rowscols = 1;
-  dims = inputdlg (prompt, "Ingrese coeficientes de G(s)", rowscols, defaults);
+  dims = inputdlg(prompt, "Ingrese coeficientes de G(s)", rowscols, defaults);
 endfunction
