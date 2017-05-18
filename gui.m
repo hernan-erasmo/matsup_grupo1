3fc5dee8a7ac6302f8498094aa741f0573d3c310
Hernán Erasmo <dherasmo@gmail.com>
2017-05-18 14:16:50 -0300

    Evita borrar la barra de menú al redibujar el plot
    Siempre que se invocaba a 'clf' se borraba toda la figura, pero ahora
    se borra todo excepto el menú principal, que sigue mostrándose y
    es funcional.

--- gui.m
+++ gui.m
@@ -1,14 +1,13 @@
 source("funciones.m");
 
-% Crea la barra de menu
-
 % Deshabilita los botones de menu que vienen por defecto
 f = figure("MenuBar", "None");
 
-btn_ingresar = uimenu(f, "label", "Ingresar nueva G(s)");
+% La propiedad handlevisibility hace que no se limpie ese widget al llamar a clf
+btn_ingresar = uimenu(f, "label", "Ingresar nueva G(s)", "handlevisibility", "off");
   btn_coeficientes = uimenu(btn_ingresar, "label", "Usando coeficientes", "callback", "dlg_ingresar_coef");
   btn_zpk = uimenu(btn_ingresar, "label", "Usando ceros, polos y ganancia");
-btn_funciones = uimenu(f, "label", "Funciones");
+btn_funciones = uimenu(f, "label", "Funciones", "handlevisibility", "off");
   btn_detalles_completos = uimenu(btn_funciones, "label", "Mostrar detalles completos");
   btn_caract_particular = uimenu(btn_funciones, "label", "Elegir caracteristica en particular");
     btn_expresion = uimenu(btn_caract_particular, "label", "Obtener expresion de G(s)", "callback", "get_expresion_gs");
@@ -18,4 +17,4 @@ btn_funciones = uimenu(f, "label", "Funciones");
     btn_pcg = uimenu(btn_caract_particular, "label", "Indicar polos, ceros y ganancia");
     btn_graf_pc = uimenu(btn_caract_particular, "label", "Graficar polos y ceros");
     btn_estabilidad = uimenu(btn_caract_particular, "label", "Indicar estabilidad");
-btn_salir = uimenu(f, "Label", "Salir", "callback", "close(gcf)");
+btn_salir = uimenu(f, "Label", "Salir", "handlevisibility", "off", "callback", "close(gcf)");
